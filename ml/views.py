c87309e7ea9edfa28543a85d9aba5ba550642b70
Enayat Kareem <75158477+KLAU-S@users.noreply.github.com>
2022-09-24 23:56:27 +0530

    Update views.py

--- ml/views.py
+++ ml/views.py
@@ -49,7 +49,7 @@ def home(response):
     a=[response.POST['os'],response.POST['al'],response.POST['pc'], response.POST['sw'], response.POST['cn'], response.POST['ec'], response.POST['coa'], response.POST['mt'], response.POST['cs'], response.POST['hr'], response.POST['lq'], response.POST['hack'], response.POST['csr'], response.POST['psp'], response.POST['cw'], response.POST['cl'], response.POST['ex']]
     model = pickle.load(open(r"ml/predictionsmodel.sav", "rb"))
     c = [int(i) for i in a]
-    predictedJob = model.predict([a])
+    predictedJob = model.predict([c])
     import math
     y = predictedJob[0]
     x = predictedJob[0]
